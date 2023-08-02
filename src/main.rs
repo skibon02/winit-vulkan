bc91b4f13a6bd28898b2f07a0a67f4bfdbfd89d6
SkyGrel19 <skibav2002@gmail.com>
2023-08-03 00:19:20 +0300

    handle events asyncronously

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use winit::{
     window::WindowBuilder,
 };
 
+
 pub mod helpers;
 
 pub mod app;
@@ -14,27 +15,87 @@ use app::App;
 
 pub mod resource_manager;
 
+enum MsgToHandler {
+    Event(Event<'static, ()>),
+    Exit,
+}
+
 fn main() {
     simple_logger::init_with_level(log::Level::Info).unwrap();
+    console_subscriber::init();
     info!("Hello, world!");
+
+
+    //init tokio runtime
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let (event_tx, mut event_rx) = tokio::sync::mpsc::unbounded_channel();
+    
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().with_title("Winit hello!").build(&event_loop).unwrap();
+    let main_window_id = window.id();
 
-    let mut app = App::new(&window).unwrap();
+    rt.block_on(async {
+        let mut app = App::new(&window).unwrap();
+
+        rt.spawn(async move {
+            loop {
+                let event = event_rx.recv().await.unwrap();
+                // println!("Received event: {:?}", event);
+                // println!("On thread {:?}", std::thread::current().id());
+
+                if let MsgToHandler::Event(event) = event {
+                    match event {
+                        Event::WindowEvent {
+                            event: WindowEvent::Resized(size),
+                            window_id,
+                        } if window_id == main_window_id => {
+                            println!("Resized to {:?}", size);
+                        }
+                        Event::MainEventsCleared => {
+                            println!("Main events cleared");
+                            app.render().unwrap();
+                        }
+                        Event::RedrawRequested(window_id) if window_id == main_window_id => {
+                            println!("Redraw requested");
+                            // app.render();
+                        }
+                        _ => (),
+                    }
+                }
+                else {
+                    break;
+                }
+
+            }
+        });
+    });
+
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Wait;
 
-        match event {
+        match &event {
             Event::WindowEvent {
                 event: WindowEvent::CloseRequested,
                 window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-            Event::MainEventsCleared => {
-                //draw here
-                app.render().unwrap();
+            } if *window_id == main_window_id => {
+
+                event_tx.send(MsgToHandler::Exit).unwrap();
+                *control_flow = ControlFlow::Exit;
             }
             _ => (),
         }
+
+        if let Some(evt) = event.to_static() {
+            if event_tx.send(MsgToHandler::Event(evt)).is_err() {
+                return
+            }
+        }
     });
+
+
 }
